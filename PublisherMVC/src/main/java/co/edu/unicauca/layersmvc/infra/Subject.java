94be5a67875797391ffd6b053f857bce87f21ffd
pdaniel <pdaniel@pop-os.localdomain>
2021-12-22 08:35:45 -0500

    conflicts solved???

--- PublisherMVC/src/main/java/co/edu/unicauca/layersmvc/infra/Subject.java
+++ PublisherMVC/src/main/java/co/edu/unicauca/layersmvc/infra/Subject.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package co.edu.unicauca.layersmvc.infra;
 
 import co.edu.unicauca.layersmvc.infra.Observer;
@@ -39,45 +38,3 @@ public abstract class Subject {
     }
 
 }
-=======
-package co.edu.unicauca.layersmvc.infra;
-
-import co.edu.unicauca.layersmvc.infra.Observer;
-import java.util.ArrayList;
-
-/**
- * Sujeto, o tambien el observadoF
- *
- * @author ahurtado
- */
-public abstract class Subject {
-
-    ArrayList<Observer> observers;
-
-    public void Subject() {
-
-    }
-
-    /**
-     * Agrega un observador
-     *
-     * @param obs
-     */
-    public void addObserver(Observer obs) {
-        if (observers == null) {
-            observers = new ArrayList<>();
-        }
-        observers.add(obs);
-    }
-
-    /**
-     * Notifica a todos los observadores que hubo un cambio en el modelo
-     */
-    public void notifyAllObserves() {
-        for (Observer each : observers) {
-            each.update(this);
-        }
-    }
-
-}
->>>>>>> 58e0cd9600f7fac917134e91aa605c302ddcdfce
